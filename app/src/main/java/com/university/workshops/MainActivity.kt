57bf8840ac5c49cae46a8113ae299170136147b6
Daniel Sergeev <danser@yandex-team.ru>
2019-11-23 14:33:36 +0300

    start workshop 4 - add login

--- app/src/main/java/com/university/workshops/MainActivity.kt
+++ app/src/main/java/com/university/workshops/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.university.workshops
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bSecondScreen.setOnClickListener { openSecondScreen() }
+    }
+
+    private fun openSecondScreen() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ workshop4_login/src/main/java/com/danser/workshop4_login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.danser.workshop4_login
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
